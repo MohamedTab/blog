77396722ce09065126f0056076728f63520810e2
MohamedTab <m.tabib@live.fr>
2016-12-22 16:05:24 +0100

    remove auth befor show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
    before_action :authenticate_user!
-   skip_before_action :authenticate_user!, only: :index
+   skip_before_action :authenticate_user!, only: [:index, :show]
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
